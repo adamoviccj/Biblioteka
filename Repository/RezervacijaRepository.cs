30fc2f977449a67b9b446990c5668e7701ff45ed
Jelena Adamovic <adamovic.sv6.2021@uns.ac.rs>
2023-08-26 17:49:24 +0200

    dodat prikaz rezervacija za clana

--- Repository/RezervacijaRepository.cs
+++ Repository/RezervacijaRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using SIMS_Projekat.Model;
+using SIMS_Projekat.View;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,9 +15,15 @@ namespace SIMS_Projekat.Repository
         public List<Rezervacija> Rezervacije { get; set; }
         public string FilePath = "../../Data/rezervacije.json";
 
+        public List<Rezervacija> RezervacijeClana { get; set; }
+
         public RezervacijaRepository()
         {
             Rezervacije = GetAllRezervacije();
+            if (LogIn.LoggedUser != null)
+            {
+                RezervacijeClana = GetAllRezervacijeClana(LogIn.LoggedUser.jmbg);
+            }
         }
 
         public List<Rezervacija> GetAllRezervacije()
@@ -29,6 +36,19 @@ namespace SIMS_Projekat.Repository
         public void Save()
         {
             File.WriteAllText(FilePath, JsonConvert.SerializeObject(Rezervacije, Formatting.Indented));
+        }
+
+        public List<Rezervacija> GetAllRezervacijeClana(string jmbg)
+        {
+            List<Rezervacija> rezervacije = new List<Rezervacija>();
+            foreach(Rezervacija rezervacija in GetAllRezervacije())
+            {
+                if (rezervacija.clan.jmbg == jmbg)
+                {
+                    rezervacije.Add(rezervacija);
+                }
+            }
+            return rezervacije;
         }
     }
 }
---
+++ View/PrikazRezervacijaClan.xaml.cs
@@ -0,0 +1,55 @@
+﻿using SIMS_Projekat.Model;
+using SIMS_Projekat.Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SIMS_Projekat.View
+{
+    /// <summary>
+    /// Interaction logic for PrikazRezervacijaClan.xaml
+    /// </summary>
+    public partial class PrikazRezervacijaClan : Window
+    {
+        public static ObservableCollection<Rezervacija> Rezervacije { get; set; }
+        private RezervacijaRepository _rezervacijaRepository { get; set; }
+        public PrikazRezervacijaClan()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            var app = Application.Current as App;
+            _rezervacijaRepository = app._rezervacijaRepository;
+
+            Rezervacije = new ObservableCollection<Rezervacija>(_rezervacijaRepository.GetAllRezervacijeClana(LogIn.LoggedUser.jmbg));
+
+        }
+
+        private void dodajRezervacijuBtn_Click(object sender, RoutedEventArgs e)
+        {
+            KreiranjeRezervacijeClan kreiranjeRezervacijeClan = new KreiranjeRezervacijeClan(null);
+            kreiranjeRezervacijeClan.Show();
+        }
+
+        public static void RefreshView(List<Rezervacija> rezervacije)
+        {
+            
+            Rezervacije.Clear();
+            foreach (Rezervacija rezervacija in rezervacije)
+            {
+                Rezervacije.Add(rezervacija);
+            }
+        }
+    }
+}
